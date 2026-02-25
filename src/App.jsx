2ff3ec7949fe2ec7ebc3682748afaf5adc2d6215
mahvelous-17 <ripxxxripyt@gmail.com>
2026-02-25 10:54:32 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  const yourName = "Andrew";
+
+  const currentYear = 2026;
+
+  const color = "black";
+
+  return (
+
+    <div>
+
+      <h1>Hello, React!</h1>
+
+      <h2>My name is {yourName}</h2>
+
+      <p>I’m learning React in {currentYear}.</p>
+
+      <p>This is my first React component!</p>
+
+      <p>My favorite color is {color}</p>
+
+    </div>
+
+  );
+
+}
+
+
+export default App;
