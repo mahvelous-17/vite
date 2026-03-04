131d132acd67761bbcfd88e1386abab330717532
mahvelous-17 <ripxxxripyt@gmail.com>
2026-03-04 11:20:26 -0800

    awg

--- src/components/Sen.jsx
+++ src/components/Sen.jsx
@@ -21,7 +21,7 @@ function Sen() {
     return (
       <div className="bg-gray-900
        border-2 
-       border-purple-500
+       border-purple-100
         rounded-2xl
          shadow-lg hover:shadow-2xl 
          hover:bg-gray-800
@@ -29,7 +29,7 @@ function Sen() {
           p-6 m-6 max-w-md
            text-center mx-auto">
         
-        <h3 className="text-3xl font-bold text-green-900 mb-4">
+        <h3 className="text-3xl font-bold text-green-400 mb-4">
           {title}
         </h3>
   
@@ -37,7 +37,7 @@ function Sen() {
           {description}
         </p>
   
-        <pre className="text-xs text-purple-900 overflow-x-auto">
+        <pre className="text-xs text-purple-100 overflow-x-auto">
           {emoji}
         </pre>
   
