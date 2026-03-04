291a416d53d8e5bf67ec4bac56ea60fb760d73c6
mahvelous-17 <ripxxxripyt@gmail.com>
2026-03-04 10:50:23 -0800

    yeller

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,10 @@ function App() {
      <Card />
      <Interest />
      <Sen />
+     <h1 className="text-4xl font-bold text-blue-600
+	bg-yellow-100 p-8 rounded-xl">
+  Tailwind is working!
+</h1>
 
       <Footer />
     </div>
